fe145a5722a7f33f2e042d5dd69faad3d99ad45d
sumanthvijay06-svg <sumanthvijay06@gmail.com>
2026-09-07 14:29:45 +0530

    Add function to move zeros in-place in a list
    Implement function to move all zeros in the list to the end while maintaining the order of non-zero elements.

---
+++ Move zeros.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        insert_pos = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
+                insert_pos += 1
